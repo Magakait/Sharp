f8e8b07208aef060a3fd6431bfc1beb92c20b53f
TiredOfCoding <tiredofcoding@outlook.com>
2018-07-23 20:50:40 +0300

    Redesigned Theme layout

--- Assets/Scripts/Settings/ThemeSettings.cs
+++ Assets/Scripts/Settings/ThemeSettings.cs
@@ -1,30 +1,14 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ThemeSettings : MonoBehaviour
 {
     public JsonFile file;
-    public InputField inputCode;
-
-    public ColorLoader ColorLoader { get; set; }
 
     private void Awake() =>
         file.Load(Constants.SettingsRoot + "Theme.json");
 
     public void Defaults() =>
         file.LoadFrom(Constants.SettingsRoot + "Defaults/Theme.json");
-
-    public void SaveColor()
-    {
-        Color color;
-        ColorUtility.TryParseHtmlString("#" + inputCode.text, out color);
-
-        ColorLoader.Save(color);
-        RefreshPainters();
-    }
-
-    public void DisplayColor(ColorVariable variable) =>
-        inputCode.text = ColorUtility.ToHtmlStringRGB(variable);
 
     public void RefreshPainters()
     {
