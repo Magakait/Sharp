23257ba3ccfc7a6c95eca3aafbfeb0e804d9bd57
TiredOfCoding <tiredofcoding@outlook.com>
2018-07-27 23:24:10 +0300

    Changed collection editable consideration logic

--- Assets/Scripts/Collections/EditorManager.cs
+++ Assets/Scripts/Collections/EditorManager.cs
@@ -52,7 +52,6 @@ public class EditorManager : MonoBehaviour
 
     [Space(10)]
     public JsonFile collectionFile;
-    public BoolVariable isEditing;
 
     private void LoadCollection()
     {
--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -1,13 +1,20 @@
-﻿using UnityEngine;
+﻿using System.IO;
+
+using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayManager : MonoBehaviour
 {
+    public Button editorButton;
+
+    [Space(10)]
     public JsonFile level;
 
     private void Awake()
     {
         LevelManager.Main.LoadLevel(level);
         Cursor.visible = false;
+        editorButton.interactable = new DirectoryInfo(level.Directory).Parent.Name == "Local";
     }
 
     public void TogglePause()
