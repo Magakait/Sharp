c2e4d0bac32d7793ca5de9eb891609481365f139
TiredOfCoding <tiredofcoding@outlook.com>
2018-09-10 10:39:55 +0300

    Visual improvements

--- Assets/Scripts/Gameplay/Units/Movemements/SnakeMovement.cs
+++ Assets/Scripts/Gameplay/Units/Movemements/SnakeMovement.cs
@@ -3,7 +3,18 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Movements/Snake")]
 public class SnakeMovement : BaseMovement
 {
-    public override void Idle(MovableComponent movable) => movable.Move(movable.Direction);
+    public override void Idle(MovableComponent movable)
+    {
+        foreach (var i in new int[] { 0, 2, 1, 3 })
+        {
+            int direction = (movable.Direction + i) % 4;
+            if (movable.CanMove(direction))
+            {
+                movable.Move(direction);
+                return;
+            }
+        }
+    }
 
     public override void Move(MovableComponent movable, int direction) => movable.Move(direction);
 }
