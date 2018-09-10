7385fbffd6b3f78b4d56a99e6bc70f1e07ba2ebe
TiredOfCoding <tiredofcoding@outlook.com>
2018-09-10 12:39:13 +0300

    MovementModifier visual improvements

--- Assets/Scripts/Gameplay/Cells/MovementModifierObject.cs
+++ Assets/Scripts/Gameplay/Cells/MovementModifierObject.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
 
+using DG.Tweening;
 using Newtonsoft.Json.Linq;
 
 public class MovementModifierObject : SerializableObject
 {
+    private void Awake() =>
+        animation = gameObject.AddComponent<TweenArrayComponent>().Init
+        (
+            DOTween.Sequence().Insert
+            (
+                frameTransform
+                    .DOScale(1.25f, 1)
+            )
+                .SetLoops(-1, LoopType.Yoyo)
+                .Play()
+        );
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerObject player = collision.GetComponent<PlayerObject>();
@@ -11,11 +24,7 @@ public class MovementModifierObject : SerializableObject
             player.Movement = movements[Movement];
     }
 
-    [Space(10)]
-    [SerializeField]
-    private new SpriteRenderer renderer;
-
-    [Space(10)]
+    [Header("Gameplay")]
     [SerializeField]
     private BaseMovement[] movements;
     [SerializeField]
@@ -33,13 +42,29 @@ public class MovementModifierObject : SerializableObject
         set
         {
             movement = value;
-            renderer.sprite = sprites[Movement];
+            movementRenderer.sprite = sprites[Movement];
         }
     }
+
+    #region animation 
+
+    [Header("Animation")]
+    [SerializeField]
+    private Transform frameTransform;
+    [SerializeField]
+    private SpriteRenderer movementRenderer;
+
+    private new TweenArrayComponent animation;
+
+    #endregion
+
+    #region serialization
 
     public override void Serialize(JToken token) =>
         token["movement"] = Movement;
 
     public override void Deserialize(JToken token) =>
         Movement = (int)token["movement"];
+
+    #endregion
 }
