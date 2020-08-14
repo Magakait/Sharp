21f3bcf36674d3f4b2a45ce13f80de498867c2f9
alkaitagi <alkaitagi@outlook.com>
2020-08-14 19:41:24 +0300

    Animate zoom

--- Assets/Scripts/Gameplay/Cells/ZoomObject.cs
+++ Assets/Scripts/Gameplay/Cells/ZoomObject.cs
@@ -1,45 +1,26 @@
 using UnityEngine;
-using Sharp.Core;
-using DG.Tweening;
 using Sharp.Camera;
 using Newtonsoft.Json.Linq;
 
 namespace Sharp.Gameplay
 {
+    [RequireComponent(typeof(Animator))]
     public class ZoomObject : MonoBehaviour, ISerializable
     {
-        private void Awake()
-        {
-            animation = gameObject.AddComponent<TweenContainer>().Init
-            (
-                DOTween.Sequence().Insert
-                (
-                    frameIn.DORotate(Constants.Eulers[1], Constants.Time),
-                    frameOut.DORotate(-Constants.Eulers[1], Constants.Time)
-                ),
-                DOTween.Sequence().Insert
-                (
-                    frameIn.DOScale(0, Constants.Time)
-                ),
-                DOTween.Sequence().Insert
-                (
-                    frameOut.DOScale(0, Constants.Time)
-                )
-            );
-        }
+        private Animator animator;
+
+        private void Awake() =>
+            animator = GetComponent<Animator>();
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if (collision.GetComponent<PlayerObject>())
-            {
-                CameraManager.Zoom(45 - 15 * Zoom);
-                animation[0].Restart();
-            }
+            if (!collision.GetComponent<PlayerObject>())
+                return;
+
+            CameraManager.Zoom(45 - 15 * Zoom);
+            animator.SetTrigger("Rotate");
         }
 
-        #region gameplay
-
-        [Space(10)]
         [SerializeField]
         private int zoom;
         public int Zoom
@@ -48,33 +29,14 @@ namespace Sharp.Gameplay
             private set
             {
                 zoom = value;
-                animation[1].Play(Zoom >= 0);
-                animation[2].Play(Zoom <= 0);
+                animator.SetInteger("Zoom", Zoom);
             }
         }
-
-        #endregion
-
-        #region animation
-
-        [Space(10)]
-        [SerializeField]
-        private Transform frameIn;
-        [SerializeField]
-        private Transform frameOut;
-
-        private new TweenContainer animation;
-
-        #endregion
-
-        #region serialization
 
         public void Serialize(JToken token) =>
             token["zoom"] = Zoom;
 
         public void Deserialize(JToken token) =>
             Zoom = (int)token["zoom"];
-
-        #endregion
     }
 }
